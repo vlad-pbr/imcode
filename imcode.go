5be309d3aa05dbf6af3eadf99fd525d3a7c6089e
Vlad <vlad.pbr@gmail.com>
2021-10-30 23:29:19 +0300

    Not initial commit

--- imcode.go
+++ imcode.go
@@ -9,27 +9,56 @@ import (
 func main() {
 
 	// data to encode
-	data, err := os.Open("../imcode-samples/chrome.png")
+	data, err := os.Open("../imcode-samples/lorem.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer data.Close()
 
 	// cypher image
-	cypher, err := os.Open("../imcode-samples/weird.png")
+	cypher, err := os.Open("../imcode-samples/32.png")
 	if err != nil {
 		panic(err)
 	}
 	defer cypher.Close()
 
 	// destination file
-	out, err := os.Create("../imcode-samples/weird.out.png")
+	out, err := os.Create("../imcode-samples/32.out.png")
 	if err != nil {
 		panic(err)
 	}
+	defer out.Close()
 
 	// encode
 	if err := codec.Encode(data, cypher, out); err != nil {
+		panic(err)
+	}
+
+	// =================
+
+	// data to encode
+	coded, err := os.Open("../imcode-samples/32.out.png")
+	if err != nil {
+		panic(err)
+	}
+	defer coded.Close()
+
+	// cypher image
+	anotherCypher, err := os.Open("../imcode-samples/32.png")
+	if err != nil {
+		panic(err)
+	}
+	defer anotherCypher.Close()
+
+	// destination file
+	decoded, err := os.Create("../imcode-samples/32.decoded.png")
+	if err != nil {
+		panic(err)
+	}
+	defer decoded.Close()
+
+	// decode
+	if err = codec.Decode(coded, anotherCypher, decoded); err != nil {
 		panic(err)
 	}
 }
--- src/codec/codec.go
+++ src/codec/codec.go
@@ -31,6 +31,7 @@ func Encode(dataStream io.Reader, cypherStream io.Reader, outStream io.Writer) e
 	// encode loop
 	buf := make([]byte, 4)
 	dataLength := 0
+	bytesRead := 0
 	eof := false
 	x, y := 0, 0
 	for ; y < img.Bounds().Max.Y && !eof; y++ {
@@ -41,43 +42,51 @@ func Encode(dataStream io.Reader, cypherStream io.Reader, outStream io.Writer) e
 			values := []uint8{byte(r / 257), byte(g / 257), byte(b / 257), byte(a / 257)}
 
 			// read from data
-			bytesRead, err := dataStream.Read(buf)
-			if err != nil {
+			bytesRead, err = dataStream.Read(buf)
+			if bytesRead < 4 {
+				eof = true
+			}
 
-				// check for end of file
-				if err == io.EOF {
-					eof = true
-				} else {
+			// check for unexpected error
+			if err != nil {
+				if err != io.EOF {
 					return fmt.Errorf("could not read data byte from stream: %s", err.Error())
 				}
+			} else {
 
+				// encode data into byte
+				dataLength += bytesRead
+				for i := 0; i < bytesRead; i++ {
+					values[i] += buf[i]
+				}
+
+				// set pixel values in image
+				img.Set(x, y, color.RGBA{
+					R: values[0],
+					G: values[1],
+					B: values[2],
+					A: values[3],
+				})
 			}
-
-			// encode data into byte
-			dataLength += bytesRead
-			for i := 0; i < bytesRead; i++ {
-				values[i] += buf[i]
-			}
-
-			// set pixel values in image
-			img.Set(x, y, color.RGBA{
-				R: values[0],
-				G: values[1],
-				B: values[2],
-				A: values[3],
-			})
 		}
 	}
 
 	// ensure there's enough pixels
 	if !eof || dataLength > maxBytes {
-		fmt.Println(dataLength)
 		return fmt.Errorf("only %d bytes can be stored using provided cypher", maxBytes)
+	}
+
+	// adjust x,y coordinates
+	y -= 1
+	if x == 0 {
+		x, y = img.Bounds().Max.X-1, y-1
+	} else {
+		x -= 1
 	}
 
 	// encode metadata
 	axes := []int{x, y}
-	for metaX, metaY := img.Bounds().Max.X-metaPixels, img.Bounds().Max.Y-1; metaX < img.Bounds().Max.X; metaX++ {
+	for metaX, metaY := img.Bounds().Max.X-metaPixels, img.Bounds().Max.Y-1; metaX < img.Bounds().Max.X-1; metaX++ {
 
 		// calculate colors
 		values := [2][2]uint8{}
@@ -91,18 +100,82 @@ func Encode(dataStream io.Reader, cypherStream io.Reader, outStream io.Writer) e
 			}
 		}
 
+		fmt.Println(img.At(metaX, metaY).RGBA())
+		fmt.Println(values[0][0])
+		fmt.Println(metaX, metaY)
+		fmt.Println(img.Bounds())
+
 		// set meta pixel color
 		img.Set(metaX, metaY, color.RGBA{
-			R: values[0][0],
-			G: values[0][1],
-			B: values[1][0],
-			A: values[1][1],
+			R: 22,
+			G: 0,
+			B: 29,
+			A: 0,
 		})
+
+		fmt.Println(img.At(metaX, metaY).RGBA())
 	}
+
+	// fmt.Println(img.At(30, 30).RGBA())
+
+	// set amount of channels used in the final pixel
+	// fmt.Println(img.Bounds().Max.X-1, img.Bounds().Max.Y-1)
+	img.Set(img.Bounds().Max.X-1, img.Bounds().Max.Y-1, color.RGBA{
+		R: 0,
+		G: 0,
+		B: 0,
+		A: uint8(bytesRead),
+	})
+	// fmt.Println(img.At(img.Bounds().Max.X-1, img.Bounds().Max.Y-1).RGBA())
 
 	// write to given out stream
 	if err = png.Encode(outStream, img); err != nil {
 		return fmt.Errorf("could not encode image: %s", err.Error())
+	}
+
+	return nil
+}
+
+func Decode(codedStream io.Reader, cypherStream io.Reader, outStream io.Writer) error {
+
+	// read coded stream as image
+	codedImg, _, err := image.Decode(codedStream)
+	if err != nil {
+		return fmt.Errorf("could not decode coded image: %s", err.Error())
+	}
+
+	// read cypher as image
+	cypherImg, _, err := image.Decode(cypherStream)
+	if err != nil {
+		return fmt.Errorf("could not decode cypher image: %s", err.Error())
+	}
+
+	// ensure dimensions
+	if !codedImg.Bounds().Eq(cypherImg.Bounds()) {
+		return fmt.Errorf("provided cypher image and coded image are not equal in dimensions")
+	}
+
+	metaPixels := getMeta(codedImg)
+	dataCoordinates := []int{0, 0}
+	for metaX, metaY := codedImg.Bounds().Max.X-metaPixels, codedImg.Bounds().Max.Y-1; metaX < codedImg.Bounds().Max.X-1; metaX++ {
+
+		// decode pixel
+		r, g, b, a := codedImg.At(metaX, metaY).RGBA()
+		values := [][]uint8{{byte(r / 257), byte(g / 257)}, {byte(b / 257), byte(a / 257)}}
+
+		// calculate data pixel coordinates
+		for j := 0; j < 2; j++ {
+			for i := 0; i < 2; i++ {
+				dataCoordinates[j] += int(values[j][i])
+			}
+		}
+	}
+
+	// parse channels used on final pixel
+	_, _, _, a := codedImg.At(codedImg.Bounds().Max.X-1, codedImg.Bounds().Max.Y-1).RGBA()
+	channelsUsed := a / 257
+	if channelsUsed > 3 {
+		return fmt.Errorf("invalid metadata: channels used %d is bigger than 3", channelsUsed)
 	}
 
 	return nil
@@ -122,5 +195,5 @@ func getMeta(img image.Image) int {
 	}
 
 	// amount of pixels needed for metadata
-	return int(math.Ceil(float64(maxBound) / 510))
+	return int(math.Ceil(float64(maxBound)/510)) + 1
 }
