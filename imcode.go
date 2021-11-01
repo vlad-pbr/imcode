7c130d3dadd0ee909f0f34605658175959e62e96
Vlad Poberezhny <vlad.pbr@gmail.com>
2021-11-01 18:37:03 +0200

    feat(cli)

--- imcode.go
+++ imcode.go
@@ -1,64 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/vlad-pbr/imcode/src/codec"
+	"github.com/vlad-pbr/imcode/src/cli"
 )
 
 func main() {
 
-	// data to encode
-	data, err := os.Open("../imcode-samples/lorem.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer data.Close()
-
-	// cypher image
-	cypher, err := os.Open("../imcode-samples/32.png")
-	if err != nil {
-		panic(err)
-	}
-	defer cypher.Close()
-
-	// destination file
-	out, err := os.Create("../imcode-samples/32.out.png")
-	if err != nil {
-		panic(err)
-	}
-	defer out.Close()
-
-	// encode
-	if err := codec.Encode(data, cypher, out); err != nil {
-		panic(err)
+	if err := cli.Handle(); err != nil {
+		fmt.Fprintf(os.Stderr, "imcode: %s\n", err.Error())
+		os.Exit(1)
 	}
 
-	// =================
-
-	// data to encode
-	coded, err := os.Open("../imcode-samples/32.out.png")
-	if err != nil {
-		panic(err)
-	}
-	defer coded.Close()
-
-	// cypher image
-	anotherCypher, err := os.Open("../imcode-samples/32.png")
-	if err != nil {
-		panic(err)
-	}
-	defer anotherCypher.Close()
-
-	// destination file
-	decoded, err := os.Create("../imcode-samples/32.decoded.png")
-	if err != nil {
-		panic(err)
-	}
-	defer decoded.Close()
-
-	// decode
-	if err = codec.Decode(coded, anotherCypher, decoded); err != nil {
-		panic(err)
-	}
+	os.Exit(0)
 }
--- src/cli/cli.go
+++ src/cli/cli.go
@@ -1,1 +1,82 @@
 package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/vlad-pbr/imcode/src/codec"
+)
+
+var F_DECODE bool
+var F_FROM string
+var F_CYPHER string
+var F_TO string
+
+func Handle() error {
+
+	// define flags
+	flag.BoolVar(&F_DECODE, "decode", false, "should the input file be decoded (encoding is default)")
+	flag.StringVar(&F_FROM, "from", "", "path to input file ('-' for stdin) [required]")
+	flag.StringVar(&F_CYPHER, "cypher", "", "path to cypher image ('-' for stdin) [required]")
+	flag.StringVar(&F_TO, "to", "-", "path to output file ('-' for stdout) [required]")
+
+	// parse args
+	flag.Parse()
+
+	// check for undefined flags
+	if len(F_FROM) == 0 || len(F_CYPHER) == 0 && len(F_TO) == 0 {
+		return fmt.Errorf("one of the required flags is not defined")
+	}
+
+	// check for duplicate stdins
+	if F_FROM == "-" && F_CYPHER == "-" {
+		return fmt.Errorf("only one stdin flag can be specified")
+	}
+
+	// parse input
+	var in *os.File
+	var err error
+	if F_FROM == "-" {
+		in = os.Stdin
+	} else {
+		in, err = os.Open(F_FROM)
+		if err != nil {
+			return fmt.Errorf("error reading file %s: %s", F_FROM, err.Error())
+		}
+	}
+
+	// parse cypher
+	var cypher *os.File
+	if F_CYPHER == "-" {
+		cypher = os.Stdin
+	} else {
+		cypher, err = os.Open(F_CYPHER)
+		if err != nil {
+			return fmt.Errorf("error reading file %s: %s", F_CYPHER, err.Error())
+		}
+	}
+
+	// parse output
+	var out *os.File
+	if F_TO == "-" {
+		out = os.Stdout
+	} else {
+		out, err = os.Create(F_TO)
+		if err != nil {
+			return fmt.Errorf("error opening file %s: %s", F_CYPHER, err.Error())
+		}
+	}
+
+	// call codec
+	if F_DECODE {
+		err = codec.Decode(in, cypher, out)
+	} else {
+		err = codec.Encode(in, cypher, out)
+	}
+	if err != nil {
+		return fmt.Errorf("codec error: %s", err.Error())
+	}
+
+	return nil
+}
